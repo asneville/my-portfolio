e15806ae08c3c6f5c10e2e0fc304707654d256ce
Adrian Neville <asneville@gmail.com>
2017-09-05 22:18:08 +0100

    Python script to upload artifacts to S3, fixed for lamda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,16 +3,19 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('asneville.virtualise-me.com')
-build_bucket = s3.Bucket('asneville-portfolio-build')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('asneville.virtualise-me.com')
+    build_bucket = s3.Bucket('asneville-portfolio-build')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+     for nm in myzip.namelist():
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj,nm,
           ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Hello from Lambda'
